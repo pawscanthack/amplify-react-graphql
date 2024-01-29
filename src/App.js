4955e9f973a35ade1379ddaf0fa2300401101353
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2024-01-28 22:55:02 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
+import image from './images/peaceful_rick.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey Hacker, meet React!
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is fun!
         </a>
       </header>
     </div>
